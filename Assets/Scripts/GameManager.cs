526e8b897759d99088587e2c9e1f29252139a4fe
Murilo Trigo <murilotrigopaz@gmail.com>
2017-10-08 17:33:53 -0400

    Adds GameManager and Ship

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Editor Side Dependency Injection
+    public Ship shipPrefab;
+
+    private Ship PlayerShip { get; set; }
+
+	void Start ()
+    {
+        PlayerShip = Instantiate(shipPrefab);
+	}
+
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            PlayerShip.Accelerate();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            PlayerShip.TurnLeft();
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            PlayerShip.TurnRight();
+        }
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    private static float ACCELERATION_RATIO = 4f;
+    private static float TURN_SPEED = 2f;
+
+    public void Accelerate()
+    {
+        GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * ACCELERATION_RATIO);
+    }
+
+    public void TurnLeft()
+    {
+        GetComponent<Rigidbody2D>().rotation += TURN_SPEED;
+    }
+
+    public void TurnRight()
+    {
+        GetComponent<Rigidbody2D>().rotation -= TURN_SPEED;
+    }
+}
