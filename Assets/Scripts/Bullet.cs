43a211fbc745f5f6f3d99b4f93ee0c1cbb8eec66
Murilo Trigo <murilotrigopaz@gmail.com>
2017-10-15 02:40:58 -0400

    Adds primitive bullet interations and animation

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("WrapAround"))
+        {
+            Destroy(collision.gameObject);
+        }
 
+        Destroy(gameObject);
+    }
 }
